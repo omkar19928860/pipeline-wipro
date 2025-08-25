b2b280034a2d9d65bf22849434c96c66c39e13bf
omkar19928860 <135116871+omkar19928860@users.noreply.github.com>
2025-08-25 14:01:00 +0530

    Update provider.tf

--- Dev/provider.tf
+++ Dev/provider.tf
@@ -7,14 +7,12 @@ terraform {
         }
     }
     backend "azurerm" {
-        resource_group_name  = "omkarrg"
-        storage_account_name = "omkarst345"
-        container_name       = "tfstate"
-        key                  = "dev.todoapp.tfstate"
+                
       
     }
 }
 provider "azurerm" {
     features {}
     subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
+
 }
