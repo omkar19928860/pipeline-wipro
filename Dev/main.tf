dd95f541b84e03df9921352ec13dc29de7565ebc
omkar <omkar@example.com>
2025-08-25 07:25:18 +0000

    4

--- Dev/main.tf
+++ Dev/main.tf
@@ -1,11 +1,11 @@
 module "rg1" {
-    source = "../../child/resource_group"
+    source = "../child/resource_group"
     rg_name = "rg-dev-omkar"
     location = "East US"
 }
   
 module "rg2" {
-    source = "../../child/resource_group"
+    source = "../child/resource_group"
     rg_name = "rg-dev-omkar2"
     location = "East US"
  
